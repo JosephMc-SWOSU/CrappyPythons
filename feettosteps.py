8d36ae99b6d4bb95108af8b591794d77edf88969
Joseph McPhearson <mcphearsonj@student.swosu.edu>
2024-10-09 10:38:29 -0500

    Worked on the step counter

--- feettosteps.py
+++ feettosteps.py
@@ -1,11 +1,30 @@
-feet = float(input("Please enter the the distance walked in feet: "))
+def print_greeting():
+    print("Welcome to the Feet to Steps Program!")
 
-def FeetToSteps(feet):
+def get_user_distance():
+    feet = float(input("Please enter the the distance walked in feet: "))
+    return feet
+
+def covert_feet_to_steps(feet):
     return feet / 2.5
 
-steps = FeetToSteps(feet)
+def print_results(steps):
+    if steps >= 2000:
+        print("You have walked", steps, "steps. You have reached your goal! Great job!")
+    else:
+        print("You have walked", steps, "steps. You have not reached your goal yet. Keep going!")
 
-if steps >= 2000:
-    print("You have walked", steps, "steps. You have reached your goal! Great job!")
-else:
-    print("You have walked", steps, "steps. You have not reached your goal yet. Keep going!")
+def print_farewell():
+    print("Thank you for using the Feet to Steps Program. Goodbye!")
+    
+if __name__ == "__main__":
+    print_greeting()
+
+    feet = get_user_distance()
+    # print("You have entered", feet, "feet.")
+    steps = covert_feet_to_steps(feet)
+    # print("after the steps print")
+    print_results(steps)
+
+    print_farewell()    
+
